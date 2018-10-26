c47fba0e9b0f9d63a3eaa545d5b2061d888738d4
Yan Wu <github_user@users.noreply.github.com>
2018-10-26 14:24:31 -0400

    basic.js updated

--- src/renderers/base.js
+++ src/renderers/base.js
@@ -4,6 +4,19 @@ import { mat4, vec4, vec3 } from 'gl-matrix';
 
 export const MAX_LIGHTS_PER_CLUSTER = 100;
 
+function count_x_distance(light_pos, w){
+    let distance = vec3.fromValues(1.0 / Math.sqrt(w * w + 1), 0 , - w / Math.sqrt(w * w + 1));
+    distance = vec3.dot(vec3.fromValues(light_pos[0], light_pos[1], light_pos[2]), distance);
+    return distance;
+}
+
+function count_y_distance(light_pos, w){
+    let distance = vec3.fromValues(0, 1.0 / Math.sqrt(w * w + 1), - w / Math.sqrt(w * w + 1));
+    distance = vec3.dot(vec3.fromValues(light_pos[0], light_pos[1], light_pos[2]), distance);
+    return distance;
+}
+
+
 export default class BaseRenderer {
   constructor(xSlices, ySlices, zSlices) {
     // Create a texture to store cluster data. Each cluster stores the number of lights followed by the light indices
@@ -27,11 +40,11 @@ export default class BaseRenderer {
       }
     }
 
-    var yz = Math.tan(0.5 * camera.fov * Math.PI / 180.0) * 2;
+    var yz = Math.tan(0.5 * camera.fov * Math.PI / 180.0);
     var xz = camera.aspect * yz;
     var z_step = (camera.far - camera.near) / this._zSlices;
-    var x_step = xz / this._xSlices;
-    var y_step = yz / this._ySlices;
+    var x_step = 2.0 * xz / this._xSlices;
+    var y_step = 2.0 * yz / this._ySlices;
 
 
     for(let light_index = 0; light_index < NUM_LIGHTS; light_index++){
@@ -43,7 +56,7 @@ export default class BaseRenderer {
       light_pos[3] = 1.0;
 
       vec4.transformMat4(light_pos, light_pos, viewMatrix);
-      pos[2] *= -1.0;
+      light_pos[2] *= -1.0;
       let radius = scene.lights[light_index].radius;
 
       // find cluster range
@@ -56,10 +69,11 @@ export default class BaseRenderer {
           break;
         }
       }
-      for(z_max = this._zSlices - 1; z > z_min; z_max--){
+      for(z_max = this._zSlices - 1; z_max > z_min; z_max--){
         let distance = (camera.near + z_max * z_step) - light_pos[2];
         if(distance <= radius){
           z_max = Math.min(this._zSlices, z_max + 1);
+          break;
         }
       }
 
@@ -67,17 +81,17 @@ export default class BaseRenderer {
 
       for(x_min = 0; x_min < this._xSlices; x_min++){
         let distance;
-        let w = x_step * (x_min + 1 - this._xSlices * 0.5);
-        distance = (light_pos[0] - w * light_pos[2]) / Math.sqrt(w * w + 1.0);
-        if(distance <= radius){
+        let w = -xz + x_min * x_step;
+        distance = count_x_distance(light_pos, w);
+        if(Math.abs(distance) <= radius){
           break;
         }
       }
       for(x_max = this._xSlices - 1; x_max > x_min; x_max--){
         let distance;
-        let w = x_step * (x_max - 1 - this._xSlices * 0.5);
-        distance = (light_pos[0] - w * light_pos[2]) / Math.sqrt(w * w + 1.0);
-        if(-1 * distance < radius){
+        let w = -xz + x_max * x_step;
+        distance = count_x_distance(light_pos, w);
+        if(Math.abs(distance) < radius){
           break;
         }
       }
@@ -85,17 +99,17 @@ export default class BaseRenderer {
       let y_min, y_max;
       for(y_min = 0; y_min < this._ySlices; y_min++){
         let distance;
-        let w = y_step * (y_min + 1 - this._ySlices * 0.5);
-        distance = (light_pos[1] - w * light_pos[2]) / Math.sqrt(w * w + 1.0);
-        if(distance <= radius){
+        let w = -yz + y_min * y_step;
+        distance = count_y_distance(light_pos, w);
+        if(Math.abs(distance) <= radius){
           break;
         }
       }
       for(y_max = this._ySlices - 1; y_max > x_min; y_max--){
         let distance;
-        let w = y_step * (y_max - 1 - this._ySlices * 0.5);
-        distance = (light_pos[1] - w * light_pos[2]) / Math.sqrt(w * w + 1.0);
-        if(-1 * distance < radius){
+        let w = -yz + y_max * y_step;
+        distance = count_y_distance(light_pos, w);
+        if(Math.abs(distance) <= radius){
           break;
         }
       }
@@ -106,22 +120,17 @@ export default class BaseRenderer {
             let i = x + y * this._xSlices + z * this._xSlices*this._ySlices;
             let count = this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)];
             count++;
-            if(count < MAX_LIGHTS_PER_CLUSTER){
+            if(count <= MAX_LIGHTS_PER_CLUSTER){
                 this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)] = count;
                 let floor = Math.floor(count / 4.0);
                 let i_new = this._clusterTexture.bufferIndex(i, floor);
                 this._clusterTexture.buffer[i_new + (count - floor * 4)] = light_index;
-            }
-
+            }else break;
           }
         }
       }
 
     }
-
-
-
-
 
     this._clusterTexture.update();
   }
